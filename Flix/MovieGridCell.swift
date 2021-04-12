7118aed0bc6a8da7839ea5adc6b412dcdf2499c1
Kelly Lin <lin400@umail.ucsb.edu>
2021-04-11 17:02:24 -0700

    lab2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kelly Lin on 4/11/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
